4a0cc28d3284ef7e6cce31c34fd6c7d242f7805e
q1618 <qinmh@dtdream.com>
2019-10-14 09:55:32 +0800

    Initial commit

---
+++ leeCodePartOne/src/ReadMe.java
@@ -0,0 +1,32 @@
+
+
+/*
+ *
+ *     @author Qmh
+ *     @Date 2019/10/12 15:24
+ *
+ *
+ */
+
+/**
+ *
+ *        这是一个个人的算法学习过程，从2019年10月12日开始
+ *        每周都会进行相关算法的习题，督促自己解答，如果有兴趣，
+ *        也可以一起做一做相关的题目。题目早期来源打算从leecode上
+ *        ，难度也分为简单，中等，困难，每次来三道题目。
+ *        督促你我共同进步
+ *
+ *        This is a personal algorithmic learning process that begins on October 12, 2019.
+ *    Weekly exercises on related algorithms, urge yourself to answer, if interested,
+ *    You can also do related topics together. Early source of the topic is intended from leecode
+ *    includes easy, medium, difficult, three questions each time.
+ *    urge you and me to make progress together
+ *
+ *
+ *
+ */
+
+public class ReadMe {
+
+
+}
